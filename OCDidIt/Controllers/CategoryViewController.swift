6d65fef6da8c9c565cb0f583d181ea184109d501
c4l1b4n <gdnbmtggg@gmail.com>
2018-09-07 18:50:42 -0700

    Added category view controller to the storyboard + set it up.
    View controller code empty.

---
+++ OCDidIt/Controllers/CategoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryViewController.swift
+//  OCDidIt
+//
+//  Created by George Dickson on 9/7/18.
+//  Copyright © 2018 George Dickson. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - TableView Data Manipulation Methods
+    
+    //MARK: - Add new Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+    //MARK: - TableView Delegate methods
+    
+}
